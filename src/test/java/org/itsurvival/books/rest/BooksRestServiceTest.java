a3144debde4ff783ce51dd9ee20aafff04c34d4b
Jakub Karbowiak <jakub.karbowiak@gmail.com>
2016-04-18 17:37:57 +0200

    extended BooksRestServiceTest

--- src/test/java/org/itsurvival/books/rest/BooksRestServiceTest.java
+++ src/test/java/org/itsurvival/books/rest/BooksRestServiceTest.java
@@ -11,16 +11,19 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -28,7 +31,7 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class BooksRestServiceTest {
 
-    @EnableAutoConfiguration
+    @EnableWebMvc
     @ComponentScan("org.itsurvival.books.rest")
     public static class BooksRestServiceTestConfiguration {
 
@@ -57,6 +60,9 @@ public class BooksRestServiceTest {
 
     @Captor
     private ArgumentCaptor<BookSearchCriteria> bookSearchCriteriaCaptor;
+
+    @Captor
+    private ArgumentCaptor<BookTo> bookCaptor;
 
     @Before
     public void setUp() {
@@ -102,6 +108,71 @@ public class BooksRestServiceTest {
         assertThat(bookSearchCriteria.getAuthor()).isNull();
     }
 
+    @Test
+    public void shouldAddNewBook() throws Exception {
+
+        // given
+        byte[] content = readFileToBytes("classpath:org/itsurvival/books/rest/newBook.json");
+
+        when(bookService.addBook(any(BookTo.class))).thenAnswer(args -> args.getArguments()[0]);
+
+        // when
+        mockMvc.perform(post("/services/book").content(content).contentType(MediaType.APPLICATION_JSON))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Test title")));
+
+
+        verify(bookService).addBook(bookCaptor.capture());
+        BookTo book = bookCaptor.getValue();
+        assertThat(book.getTitle()).isEqualTo("Test title");
+        assertThat(book.getAuthor()).isEqualTo("Test author");
+        assertThat(book.getYear()).isEqualTo(2008);
+        assertThat(book.getGenre()).isEqualTo(Genre.IT);
+        assertThat(book.getVersion()).isEqualTo(0L);
+        assertThat(book.getId()).isNull();
+    }
+
+    @Test
+    public void shouldUpdateExistingBook() throws Exception {
+
+        // given
+        byte[] content = readFileToBytes("classpath:org/itsurvival/books/rest/existingBook.json");
+
+        when(bookService.updateBook(any(BookTo.class))).thenAnswer(args -> args.getArguments()[0]);
+
+        // when
+        mockMvc.perform(put("/services/book").content(content).contentType(MediaType.APPLICATION_JSON))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Test title")));
+
+        verify(bookService).updateBook(bookCaptor.capture());
+        BookTo book = bookCaptor.getValue();
+        assertThat(book.getTitle()).isEqualTo("Test title");
+        assertThat(book.getAuthor()).isEqualTo("Test author");
+        assertThat(book.getYear()).isEqualTo(2008);
+        assertThat(book.getGenre()).isEqualTo(Genre.IT);
+        assertThat(book.getVersion()).isEqualTo(2L);
+        assertThat(book.getId()).isEqualTo(20931L);
+    }
+
+    @Test
+    public void shouldDeleteBook() throws Exception {
+        // given
+        long bookId = 1L;
+        boolean deleteResult = true;
+        when(bookService.deleteBook(1L)).thenReturn(deleteResult);
+
+        // when
+        mockMvc.perform(delete("/services/book/1"))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is(deleteResult)));
+
+        verify(bookService).deleteBook(bookId);
+    }
+
     private BookTo createBook(Long id) {
         BookTo bookTo = new BookTo();
         bookTo.setAuthor("Test author");
@@ -111,6 +182,10 @@ public class BooksRestServiceTest {
         bookTo.setId(id);
         bookTo.setVersion(2L);
         return bookTo;
+    }
+
+    private byte[] readFileToBytes(String resourcePath) throws IOException {
+        return Files.readAllBytes(webApplicationContext.getResource(resourcePath).getFile().toPath());
     }
 
 }
