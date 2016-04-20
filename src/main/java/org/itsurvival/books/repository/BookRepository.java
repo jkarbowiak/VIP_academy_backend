153a96e157266c1f974ef80023e310dad7fb92cf
Jakub Karbowiak <jakub.karbowiak@gmail.com>
2016-04-20 17:01:04 +0200

    Removed files not needed for step-2. Server won't start because of missing BookServiceImpl.

--- src/main/java/org/itsurvival/books/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package org.itsurvival.books.repository;
-
-import org.itsurvival.books.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long>, BookSearchRepository {
-
-}
--- src/main/java/org/itsurvival/books/repository/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.itsurvival.books.repository;
-
-import org.itsurvival.books.entity.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryRepository extends JpaRepository<Library, Long> {
-}
