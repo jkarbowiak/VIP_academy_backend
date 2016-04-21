207ba90e795d536c1eaab3364347aa50a0a5bd29
Tomasz Szala <tomasz.szala@capgemini.com>
2016-04-22 00:13:16 +0200

    MappStruct removed

--- src/main/java/org/itsurvival/books/service/impl/BookServiceImpl.java
+++ src/main/java/org/itsurvival/books/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.itsurvival.books.common.BookSearchCriteria;
 import org.itsurvival.books.entity.Book;
+import org.itsurvival.books.mapper.BookMapper;
 import org.itsurvival.books.mapper.BookMapperS;
 import org.itsurvival.books.repository.BookRepository;
 import org.itsurvival.books.service.BookService;
@@ -18,59 +19,62 @@ import com.google.common.collect.Lists;
 @Service
 public class BookServiceImpl implements BookService {
 
-    private final BookRepository bookRepository;
+  private final BookRepository bookRepository;
 
-    private final BookMapperS bookMapper;
+  @Autowired
+  private BookMapperS bookMapperS;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository, BookMapperS bookMapper) {
-        this.bookRepository = bookRepository;
-        this.bookMapper = bookMapper;
+  @Autowired
+  private BookMapper bookMapper;
+
+  @Autowired
+  public BookServiceImpl(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @Override
+  public List<BookTo> findBooks(BookSearchCriteria bookSearchCriteria) {
+
+    List<Book> books = this.bookRepository.findBooks(bookSearchCriteria);
+    return this.bookMapper.mapSourceCollection(Lists.newArrayList(books));
+  }
+
+  @Override
+  public BookTo addBook(BookTo bookTo) {
+
+    return saveBook(bookTo);
+  }
+
+  @Override
+  public BookTo readBook(long id) {
+
+    Book book = this.bookRepository.findOne(id);
+    return this.bookMapper.convertToTransportObject(book);
+  }
+
+  @Override
+  public boolean deleteBook(long id) {
+
+    boolean deleted = false;
+    Book book = this.bookRepository.findOne(id);
+    if (book != null) {
+      this.bookRepository.delete(book);
+      deleted = true;
     }
+    return deleted;
+  }
 
-    @Override
-    public List<BookTo> findBooks(BookSearchCriteria bookSearchCriteria) {
+  @Override
+  public BookTo updateBook(BookTo bookTo) {
 
-        List<Book> books = this.bookRepository.findBooks(bookSearchCriteria);
-        return this.bookMapper.booksToBookTos(Lists.newArrayList(books));
-    }
+    return saveBook(bookTo);
+  }
 
-    @Override
-    public BookTo addBook(BookTo bookTo) {
+  private BookTo saveBook(BookTo bookTo) {
 
-        return saveBook(bookTo);
-    }
-
-    @Override
-    public BookTo readBook(long id) {
-
-        Book book = this.bookRepository.findOne(id);
-        return this.bookMapper.bookToBookTo(book);
-    }
-
-    @Override
-    public boolean deleteBook(long id) {
-
-        boolean deleted = false;
-        Book book = this.bookRepository.findOne(id);
-        if (book != null) {
-            this.bookRepository.delete(book);
-            deleted = true;
-        }
-        return deleted;
-    }
-
-    @Override
-    public BookTo updateBook(BookTo bookTo) {
-
-        return saveBook(bookTo);
-    }
-
-    private BookTo saveBook(BookTo bookTo) {
-
-        Book book = this.bookMapper.bookToToBook(bookTo);
-        Book savedBook = this.bookRepository.saveAndFlush(book);
-        return this.bookMapper.bookToBookTo(savedBook);
-    }
+    Book book = this.bookMapper.convertToEntity(bookTo);
+    Book savedBook = this.bookRepository.saveAndFlush(book);
+    return this.bookMapper.convertToTransportObject(savedBook);
+  }
 
 }
